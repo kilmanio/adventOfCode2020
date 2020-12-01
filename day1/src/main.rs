6a172f09d59b6380ca7563c420e942d6f3f1a706
kilmanio <f.vastenhout@live.nl>
2020-12-01 15:44:35 +0100

    day1 part1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+//use std::path::Path;
+
+fn populate() -> Vec<u32> {
+    let mut v: Vec<u32> = Vec::new();
+    let file = File::open("input").unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        v.push(line.unwrap().parse::<u32>().unwrap());
+    }
+
+    v
 }
+
+fn resolve(numbers: Vec<u32>) {
+    let goal = 2020;
+    let mut result = 0;
+
+    for number in &numbers {
+        let target = goal - number;
+        if numbers.contains(&target) {
+            result = number * target;
+            break;
+        }
+    }
+    println!("{}", result);
+}
+
+fn main() {
+    //let numbers = populate();
+    let numbers: Vec<u32>;
+    numbers = populate();
+    //populate2(&mut numbers);
+    resolve(numbers);
+}
+
